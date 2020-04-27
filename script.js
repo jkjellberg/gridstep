ea62bb2657aa0e28d0a47bc4dff52e47aef7bde1
Johannes Kjellberg <johannes.kjellberg@gmail.com>
2020-04-27 13:14:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let inputs = document.querySelectorAll(".st0")
 // for every circle
 inputs.forEach(i =>{
   //when the circle is clicked
-  i.addEventListener("touchmove", ()=>{
+  i.addEventListener("click", ()=>{
    // toggle the class checked on the clicked one
    i.classList.toggle("checked")
 })
