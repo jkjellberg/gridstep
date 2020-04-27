1d10c20622aaf04c174b5df3f48d7ee9639b5fff
Johannes Kjellberg <johannes.kjellberg@gmail.com>
2020-04-27 14:25:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let inputs = document.querySelectorAll(".st0")
 // for every shape
 inputs.forEach(i =>{
   //when the shape is clicked
-  i.addEventListener("touchenter", ()=>{
+  i.addEventListener("pointerenter", ()=>{
    // toggle the class checked on the clicked one
    i.classList.toggle("checked")
 })
