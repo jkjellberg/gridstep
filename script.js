588e4625b017a6955239cc156dca482afaf17503
Johannes Kjellberg <johannes.kjellberg@gmail.com>
2020-04-27 13:07:06 +0200

    Grid is toucable

---
+++ script.js
@@ -0,0 +1,10 @@
+// selects all the circles with a class of radio
+let inputs = document.querySelectorAll(".st0")
+// for every circle
+inputs.forEach(i =>{
+  //when the circle is clicked
+  i.addEventListener("touchmove", ()=>{
+   // toggle the class checked on the clicked one
+   i.classList.toggle("checked")
+})
+})
