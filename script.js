5711988bd1b111cd1a6b84c38ebc838298a5da40
Johannes Kjellberg <johannes.kjellberg@gmail.com>
2020-04-30 14:12:26 +0200

    sequencer now triggers on time and not so sloppy

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ document.documentElement.addEventListener("mousedown", () => {
 });
 
 // selects all the shapes with a class st0
-//const inputs = document.querySelectorAll(".st0");
+//const trigs = document.querySelectorAll(".st0");
 // for every shape
-//inputs.forEach(i =>{
+//trigs.forEach(i =>{
 //when the shape is clicked
 //i.addEventListener("click", ()=>{
 // toggle the class checked on the clicked one
@@ -19,28 +19,28 @@ document.documentElement.addEventListener("mousedown", () => {
 
 //Lets the user start the 'swipe' outside of the grid as long as they start it in the body
 document.body.addEventListener("pointerdown", (e) => {
-  document.body.releasePointerCapture(e.pointerId); // 
+  document.body.releasePointerCapture(e.pointerId); //
 });
 
 // selects all shapes with class st0 (all the steps)
-const inputs = document.querySelectorAll(".st0");
+const trigs = document.querySelectorAll(".st0");
 
-inputs.forEach((i) => {
+trigs.forEach((trig) => {
   //adds a pointer down listerner to each step to be able to release the target
-  i.addEventListener("pointerdown", (e) => {
+  trig.addEventListener("pointerdown", (e) => {
     //console.log("down");
     //console.log("attempt release implicit capture");
-    i.releasePointerCapture(e.pointerId); // <- Important!
+    trig.releasePointerCapture(e.pointerId); // <- Important!
   });
 
   //adds a pointerenter event listener to all steps
-  i.addEventListener("pointerenter", (e) => {
+  trig.addEventListener("pointerenter", (e) => {
     //console.log("enter");
     // add the class checked if mouse/fingers enters shape (doesn't care if mouse is down atm...)
-    i.classList.toggle("checked");
+    trig.classList.toggle("checked");
   });
   // we don't need this at the moment, but adds a listener for when the finger/pointer leaves the shape
-  i.addEventListener("pointerleave", (e) => {
+  trig.addEventListener("pointerleave", (e) => {
     //console.log("leave");
   });
 });
@@ -62,12 +62,12 @@ function repeat(time) {
   let step = index % 16;
 
   // remove the class active_step from the previous step and adds it to the active step
-  inputs[previous_step].classList.remove("active_step");
-  inputs[step].classList.toggle("active_step");
+  trigs[previous_step].classList.remove("active_step");
+  trigs[step].classList.toggle("active_step");
 
   // if the active step is cheked a note will be played.
-  if (inputs[step].classList.contains("checked"))
-    synth.triggerAttackRelease("g2", "16n");
+  if (trigs[step].classList.contains("checked"))
+    synth.triggerAttackRelease("g2", "16n", time);
 
   previous_step = step;
   index++;
