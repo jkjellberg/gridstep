f410250944bd4072bae041e2668999a036a2f35a
Johannes Kjellberg <johannes.kjellberg@gmail.com>
2020-04-30 13:55:51 +0200

    Lets the user start the 'swipe' outside of the grid as long as they start it in the body

--- script.js
+++ script.js
@@ -1,70 +1,73 @@
 //fixes audio problem in chrome
-document.documentElement.addEventListener('mousedown', () => {
-  if (Tone.context.state !== 'running')
-  Tone.context.resume();
+document.documentElement.addEventListener("mousedown", () => {
+  if (Tone.context.state !== "running") Tone.context.resume();
 });
 
 // selects all the shapes with a class st0
 //const inputs = document.querySelectorAll(".st0");
 // for every shape
 //inputs.forEach(i =>{
-  //when the shape is clicked
-  //i.addEventListener("click", ()=>{
-   // toggle the class checked on the clicked one
-   //i.classList.toggle("checked");
-   //play a middle 'C' for the duration of an 8th note
-   //var synth = new Tone.Synth().toMaster();
-   //synth.triggerAttackRelease('C4', '8n');
+//when the shape is clicked
+//i.addEventListener("click", ()=>{
+// toggle the class checked on the clicked one
+//i.classList.toggle("checked");
+//play a middle 'C' for the duration of an 8th note
+//var synth = new Tone.Synth().toMaster();
+//synth.triggerAttackRelease('C4', '8n');
 //})
 //})
 
-// selects all shapes with class sto (all the steps)
-let inputs = document.querySelectorAll(".st0");
+//Lets the user start the 'swipe' outside of the grid as long as they start it in the body
+document.body.addEventListener("pointerdown", (e) => {
+  document.body.releasePointerCapture(e.pointerId); // 
+});
 
-inputs.forEach(i =>{
+// selects all shapes with class st0 (all the steps)
+const inputs = document.querySelectorAll(".st0");
+
+inputs.forEach((i) => {
   //adds a pointer down listerner to each step to be able to release the target
-  i.addEventListener("pointerdown",(e)=>{
+  i.addEventListener("pointerdown", (e) => {
     //console.log("down");
     //console.log("attempt release implicit capture");
     i.releasePointerCapture(e.pointerId); // <- Important!
-  })
+  });
 
   //adds a pointerenter event listener to all steps
-  i.addEventListener("pointerenter",(e)=>{
+  i.addEventListener("pointerenter", (e) => {
     //console.log("enter");
     // add the class checked if mouse/fingers enters shape (doesn't care if mouse is down atm...)
-     i.classList.toggle("checked");
-  })
+    i.classList.toggle("checked");
+  });
   // we don't need this at the moment, but adds a listener for when the finger/pointer leaves the shape
-  i.addEventListener("pointerleave",(e)=>{
+  i.addEventListener("pointerleave", (e) => {
     //console.log("leave");
-  })
-})
+  });
+});
 
 //Creates new synth and sends it to master
-var synth = new Tone.Synth().toMaster();
+const synth = new Tone.Synth().toMaster();
 
 // variables for the stepsequencer
 let index = 0;
 let previous_step = 0;
 
 // Initialize the time, will call function repeat each 16ths note. 120 bpm atm.
-Tone.Transport.scheduleRepeat(repeat, '16n');
+Tone.Transport.scheduleRepeat(repeat, "16n");
 //Start the function above
 Tone.Transport.start();
 
-
 // This will happen every 16th note
-function repeat(time){
+function repeat(time) {
   let step = index % 16;
 
   // remove the class active_step from the previous step and adds it to the active step
   inputs[previous_step].classList.remove("active_step");
   inputs[step].classList.toggle("active_step");
 
-  // if the active step is cheked a note will be played. 
+  // if the active step is cheked a note will be played.
   if (inputs[step].classList.contains("checked"))
-    synth.triggerAttackRelease('g2', '16n');
+    synth.triggerAttackRelease("g2", "16n");
 
   previous_step = step;
   index++;
